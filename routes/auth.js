eb0e61b598af0967b3280f7a62a2fe2767bdb597
ashns <35615496+ashns@users.noreply.github.com>
2022-04-10 01:29:31 -0400

    updated

--- routes/auth.js
+++ routes/auth.js
@@ -2,5 +2,5 @@ const express=require("express");
 const authController=require("../controllers/auth")
 const router=express.Router();
 
-
+router.post("/createAccount", authController.createAccount)
 module.exports = router;
