fdb2788b8a434df2b151716fdec293355fbec61a
Seth Santos <sethsantos789@gmail.com>
2022-04-10 09:33:01 -0400

    Update app.js
    Added insert into results table

--- app.js
+++ app.js
@@ -76,9 +76,12 @@ app.listen(5001, () => {
     console.log("Sever started on Port 5001")
 });
 
+let user
+
 app.post('/auth/index', function(req, res) {
     const {username, password} = req.body;
     if (username && password) {
+        user = username
         db.query(`SELECT username,password FROM account WHERE username = '${username}' AND password = '${password}'`, function(error, results, fields) {
             if (results.rowCount > 0) {
                 req.session.loggedin = true;
@@ -117,6 +120,8 @@ app.post('/results', function(req, res) {
             var temp = 0;
             var temp2 = 0;
         var queryString = "WHERE rooms = '" + rooms + "' AND bathrm = '" + bathrooms + "' AND source";
+        var insertValues = `'${user}', `
+        var insertString = "INSERT INTO quiz_results VALUES (default, " + insertValues
         if(residential==1){
             temp = 1;
         }
@@ -124,49 +129,66 @@ app.post('/results', function(req, res) {
             temp2 = 1;
         }
         if(temp==1&&temp2==1){
-            queryString += " IN ('Residential', 'Condominium')"
+            queryString += " IN ('Residential', 'Condominium')";
+            insertString += " ('Residential', 'Condominium'),";
         }
         else if(temp==1){
             queryString += " = 'Residential'";
+            
+            insertString += " 'Residential',";
         }
         else if (temp2==1){
             queryString += " = 'Condominium'";
+            
+            insertString += " 'Condominium',";
         }
         if(units==0){
             queryString += " AND num_units = '1'";
+            
         }
+        insertString += ` '${rooms}', '${bathrooms}', '${units}', `;
         queryString += " AND cndtn";
+        let conditions = ['Poor', 'Fair', 'Average', 'Good', 'Very Good', 'Excellent']
         if(condition==1){
             queryString += " IN ('Poor', 'Fair', 'Average', 'Good', 'Very Good', 'Excellent')";
+            insertString += `'${conditions[condition-1]}'`;
         }
         else if(condition==2){
             queryString += " IN ('Fair', 'Average', 'Good', 'Very Good', 'Excellent')";
+            insertString += `'${conditions[condition-1]}'`;
         }
         else if(condition==3){
-            queryString += " IN ('Average', 'Good', 'Very Good', 'Excellent')";   
+            queryString += " IN ('Average', 'Good', 'Very Good', 'Excellent')";  
+            insertString += `'${conditions[condition-1]}'`;
         }
         else if(condition==4){
             queryString += " IN ('Good', 'Very Good', 'Excellent')";      
+            insertString += `'${conditions[condition-1]}'`;
         }
         else if(condition==5){
             queryString += " IN ('Very Good', 'Excellent')";
+            insertString += `'${conditions[condition-1]}'`;
         }
         else if(condition==6){
             queryString += "= 'Excellent'";
+            insertString += `'${conditions[condition-1]}'`;
         }
         queryString += " LIMIT 30";
+        insertString += ')'
         console.log(queryString);
     db.query('SELECT fulladdress, rooms, bathrm, num_units, zipcode, source, cndtn FROM staging ' + queryString, function(error, results, fields){
         if(error){
             console.log(error);
         }
-        var listings = results;
-        if (listings) {
-          return res.render('results', {listings: listings.rows});
+        var user = results;
+        if (user) {
+          return res.render('results', {user: user.rows});
         } else {
             return res.render('results', {message: 'Listings not found!'});
         }
     });
+    console.log(insertString)
+    db.query(insertString);
 }
 else{
     res.send("Please login!");
